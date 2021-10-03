0013427627447eb4aefbba0f94cd04b9a5e52963
Justin Eldridge <eldridgejm@gmail.com>
2021-10-03 12:45:54 -0700

    Added --remove-completely.

--- removesoln.py
+++ removesoln.py
@@ -1,4 +1,4 @@
-"""Script to remove the contents of soln environments in a homework."""
+"""Script to remove soln environments in a homework."""
 from textwrap import dedent
 import argparse
 import re
@@ -25,9 +25,13 @@ def get_leading_whitespace(s):
     return re.search(r'^(\s*)\S', s).groups()[0]
 
 
-def make_blank_solution_node(leading_whitespace):
+def make_blank_solution_node(leading_whitespace, remove_completely=False):
     """Create a blank solution node with nice indentation."""
-    lines = REPLACE_WITH.split('\n')
+    if not remove_completely:
+        lines = REPLACE_WITH.split('\n')
+    else:
+        return ''
+
     for i in range(1, len(lines)):
         lines[i] = leading_whitespace + lines[i]
     return TexSoup('\n'.join(lines)).find(SOLUTION_ENVIRONMENT_NAME)
@@ -36,6 +40,7 @@ def make_blank_solution_node(leading_whitespace):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('input')
+    parser.add_argument('--remove-completely', action='store_true')
     args = parser.parse_args()
 
     with open(args.input) as fileobj:
@@ -46,7 +51,7 @@ def main():
     for node in document.find_all(SOLUTION_ENVIRONMENT_NAME):
         lines = str(node).split('\n')
         leading_whitespace = get_leading_whitespace(lines[-1])
-        blank_node = make_blank_solution_node(leading_whitespace)
+        blank_node = make_blank_solution_node(leading_whitespace, args.remove_completely)
         node.replace_with(blank_node)
 
     print(document)
